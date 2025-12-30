dce111d2101fa40788dbf49caf7c349f0bb83011
Freetoplay951 <schule.paul89@gmail.com>
2025-12-30 17:08:50 +0100

    updated cmd

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-git pull && yarn && yarn build && rmdir /S /Q fivem\build && mkdir fivem\build && xcopy dist fivem\build /E
+git pull && yarn && yarn build && if exist fivem\build rmdir /S /Q fivem\build && mkdir fivem\build && xcopy dist fivem\build /E /I /Y
