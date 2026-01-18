bac9a0d5c9f0998584fccac90d89737c3f8defa4
Freetoplay951 <schule.paul89@gmail.com>
2026-01-18 15:51:10 +0100

    -

--- fivem/client/minimap.lua
+++ fivem/client/minimap.lua
@@ -1,25 +1,15 @@
--- Minimap Shape Enum
 MinimapShape = {
     SQUARE = "square",
     ROUND = "round"
 }
 
--- Set minimap shape using GTA natives
 local function SetMinimapShape(shape)
     print("Shape changed to " .. tostring(shape))
-    
+
     if shape == MinimapShape.ROUND then
-        -- Round minimap (like in certain missions)
-        SetMinimapComponentPosition("minimap", "L", "B", 0.0, -0.047, 0.1638, 0.183)
-        SetMinimapComponentPosition("minimap_mask", "L", "B", 0.0, 0.0, 0.128, 0.20)
-        SetMinimapComponentPosition("minimap_blur", "L", "B", -0.01, 0.025, 0.262, 0.300)
-        SetRadarBigmapEnabled(false, false)
+        SetMinimapClipType(1)
     else
-        -- Square minimap (default GTA style)
-        SetMinimapComponentPosition("minimap", "L", "B", 0.0, -0.047, 0.1638, 0.183)
-        SetMinimapComponentPosition("minimap_mask", "L", "B", 0.0, 0.0, 0.128, 0.20)
-        SetMinimapComponentPosition("minimap_blur", "L", "B", -0.01, 0.025, 0.262, 0.300)
-        SetRadarBigmapEnabled(false, false)
+        SetMinimapClipType(0) 
     end
 end
 
